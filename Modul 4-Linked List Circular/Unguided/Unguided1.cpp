a0026e8d0a82b6810e5f1feff9c034d100371114
Cryoschr <cryoschr@example.com>
2024-04-01 21:53:09 +0700

    circular

---
+++ Modul 4-Linked List Circular/Unguided/Unguided1.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"PROGRAM LINKED LIST CIRCULAR"<<endl;
+    cout<<"1. Tambah Depan"<<endl;
+    cout<<"2. Tambah Belakang"<<endl;
+    cout<<"3. Tambah Tengah"<<endl;
+    cout<<"4. Ubah Depan"<<endl;
+    cout<<"5. Ubah Belakang"<<endl;
+    cout<<"6. Ubah Tengah"<<endl;
+    cout<<"7. Hapus Depan"<<endl;
+    cout<<"8. Hapus Belakang"<<endl;
+    cout<<"9. Hapus Tengah"<<endl;
+    cout<<"10. Hapus List"<<endl;
+    cout<<"11. Tampilkan"<<endl;
+    cout<<"0. Keluar"<<endl;
+    cout<<endl;
+    cout<<"Pilih Operasi : "<<endl;
+    cin>>;
+    switch (){
+
+    case 1:
+// Tambah Depan
+        break;
+
+    case 2:
+// Tambah Belakang
+        break;
+
+    case 3:
+// Tambah Tengah
+        break;
+
+    case 4:
+// Ubah Depan
+        break;
+
+    case 5:
+// Ubah Belakang
+        break;
+
+    case 6:
+// Hapus Depan
+        break;
+
+    case 7:
+// Ubah Tengah
+        break;
+
+    case 8:
+// Hapus Belakang
+        break;
+
+    case 9:
+// Hapus Tengah
+        break;
+
+    case 10:
+// Hapus List
+        break;
+
+    case 11:
+// Tampilkan
+        break;
+
+    case 0:
+// Keluar
+        break;
+    
+    default:
+        break;
+    }
+return 0;
+}
